830771e76847419f70d195e98a1375b34f34d0d7
manas-sahu <manas.sahu.one@gmail.com>
2020-03-12 00:30:08 +0530

    Search functionality working

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/Layout/Navbar'
 import './App.css';
 import Users from './Components/Users/Users';
 import Axios from 'axios';
+import Search from './Components/Users/Search';
 
 
 class App extends Component {
@@ -19,11 +20,18 @@ class App extends Component {
       users: res.data
     })
   }
+  searchUsers = async text =>{
+    this.setState({loading:true});
+    console.log(text + 'From App.js');
+    const res = await Axios.get(`https://api.github.com/search/users?q=${text}`);
+    this.setState({users: res.data.items, loading:false});
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="conatiner">
+          <Search searchUsers={this.searchUsers}/>
           <Users users={this.state.users} loading={this.state.loading}/>
         </div>
       </div>
---
+++ src/Components/Users/Search.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+class Search extends Component {
+
+    state = {
+        text: ''
+    }
+    onChange = (e) =>{
+        console.log(e.target.value);
+        this.setState({text: e.target.value});
+    }
+    onSubmit = (e) =>
+    {
+        e.preventDefault();
+        this.props.searchUsers(this.state.text)
+        console.log(this.state.text);
+    }
+    render() {
+        return (
+            <div>
+                <form className="form" onSubmit={this.onSubmit}>
+                    <input type="text" name="text" placeholder="Search Users..." onChange={this.onChange} value={this.state.text} />
+                    <input type="submit" value="search" className="btn btn-dark btn-block" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Search
